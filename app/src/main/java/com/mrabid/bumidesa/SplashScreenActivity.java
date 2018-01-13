e3c60983a2717a74cec8d3942e77bd48ed8cf056
wahyuabid <wahyu.abied@gmail.com>
2018-01-13 13:23:35 +0700

    Splash finish(login  register on going)

---
+++ app/src/main/java/com/mrabid/bumidesa/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package com.mrabid.bumidesa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+    }
+}
